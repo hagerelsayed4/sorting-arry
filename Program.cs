8bdf7134d9fa3676e3cf157061207d2504c7ea68
Hager ELsayed <40064161+hagerelsayed4@users.noreply.github.com>
2018-10-10 01:06:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numbers :");
+            int[] arr = new int[8];
+            string arr1 = "";
+            for (int i = 0; i < 8; i++)
+            {
+                arr1 = Console.ReadLine();
+                arr[i] = Convert.ToInt32(arr1);
+            }
+
+            int a, b, temp = 0;
+            Console.WriteLine();
+            for (a = 0; a < arr.Length; a++)
+            {
+                for (b = a + 1; b < arr.Length; b++)
+                {
+                    if (arr[a] > arr[b])
+                    {
+                        temp = arr[a];
+                        arr[a] = arr[b];
+                        arr[b] = temp;
+                    }
+                }
+                Console.WriteLine(arr[a]);
+            }
+            Console.ReadLine();
+        }
+    }
+} 
+       
